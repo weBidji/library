60954e0fd07934291b1241baa24b6631279db6da
Bidji <159267099+weBidji@users.noreply.github.com>
2024-03-26 10:50:59 -0400

    add basic for skeleton, layout for ease of work

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+const addbtn = document.getElementById('add-btn');
+const dialog = document.querySelector("dialog");
+
+
+function Book(title, author, pages, read) {
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = Boolean(read);
+
+}
+
+addbtn.addEventListener('click', () => {
+
+})
+
+function addBookToLibrary() {
+
+}
+
